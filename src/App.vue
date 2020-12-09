a9c5225b6a4faed48f014d6975b4aa68c789e8a1
Jason Bean <jasonbean92@gmail.com>
2020-12-09 10:13:57 -0500

    Tiles Demo

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="tiles">
+    <!-- <square text="This is a square" size="small"/>
+    <square text="This is a square" size="medium"/>
+    <square text="This is a square" size="large"/>
+    <square text="This is a square" size="large" color="#000"/> -->
+    <div class="g__tile_wrapper" >
+      <square
+        v-for="data in records"
+        v-bind:key="data.text"
+        v-bind:text="data.text"
+        v-bind:href="data.href"
+        v-bind:description="data.description"
+        v-bind:size="data.size"
+        v-bind:color="data.color"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import square from './components/square.vue'
+import json from './json/data.json'
+
+export default {
+  name: 'tiles',
+  data(){
+    return{
+      records: json.records,
+      options: json.options
+    }
+  },
+  components: {
+    square
+  }
+}
+</script>
+
+<style src="./assets/tailwind.css"/>
+<style>
+.g__tile_wrapper{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+}
+
+</style>
---
+++ src/components/square.vue
@@ -0,0 +1,51 @@
+<template>
+  <a v-bind:href="href" transition="expand"  class="square rounded-lg shadow-md" :style="cssVars" v-bind:class="size">
+    <h2 v-if="!!text" v-html="text" class="text-3xl"></h2>
+    <p v-if="!!description" v-html="description"></p>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'Square',
+  props: {
+    color: String,
+    size: String,
+    href: String,
+    text: String,
+    description: String,
+    bgColor: {
+      type: String,
+      default: "#0099CC"
+    },
+    height: {
+      type: Number,
+      default: 400
+    }
+  },
+  computed: {
+    cssVars() {
+      return {
+        '--bg-color': this.bgColor,
+        '--height': this.height + 'px',
+        '--text-color': this.color
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .square {
+    background-color: var(--bg-color);
+    color: var(--text-color);
+    height: var(--height);
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
